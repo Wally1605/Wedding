ed32246c59a705aef14e699498919deb1542b0f7
Wally1605 <156972129+Wally1605@users.noreply.github.com>
2024-02-20 14:09:23 -0500

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,14 @@
+public class Person {
+    public String first;
+    public String last;
+    public Person(String first, String last){
+        this.first = first;
+        this.last = last;
+    }
+    public String getFirst(){
+        return first;
+    }
+    public String getLast(){
+        return last;
+    }
+}
